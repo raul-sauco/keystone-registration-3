d216d0684bb64c360be14c7a221fd570e555a1c7
Raul Sauco <sauco.raul@gmail.com>
2020-12-24 10:02:39 +0100

    Updated FAQ page

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -2,15 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
 import { RouteStateService } from 'src/app/services/route-state/route-state.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Faq } from 'src/app/models/faq';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { ApiService } from 'src/app/services/api/api.service';
-import { HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-faq',
@@ -18,16 +9,13 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./faq.component.scss'],
 })
 export class FaqComponent implements OnInit {
-  faq$: Observable<Faq[]>;
-  needsLogin = false;
+  packingListLink: string;
+  documentPageLink: string;
 
   constructor(
-    private api: ApiService,
     private logger: NGXLogger,
     private routeStateService: RouteStateService,
-    private route: ActivatedRoute,
-    private dialog: MatDialog,
-    public auth: AuthService
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
@@ -41,140 +29,12 @@ export class FaqComponent implements OnInit {
             this.routeStateService.updateTripIdParamState(tripId);
           }
         });
-
-        // TODO get trip id from auth user
-        this.fetch({ 'trip-id': tripId }, headers);
+        this.packingListLink = `/packing-list/${tripId}`;
+        this.documentPageLink = `/documents/${tripId}`;
       } else {
-        // No trip id parameter, try to find an user
-        this.auth.checkAuthenticated().then((res: boolean) => {
-          if (res && this.auth.getCredentials().accessToken) {
-            headers.authorization = `Bearer ${
-              this.auth.getCredentials().accessToken
-            }`;
-            this.fetch(null, headers);
-            // this.guide$ = this.guideService.fetchGuides();
-          } else {
-            this.needsLogin = true;
-          }
-        });
+        this.packingListLink = '/packing-list';
+        this.documentPageLink = '/documents';
       }
     });
-  }
-
-  /**
-   * Have the ApiService request faqs for the current user's
-   * trip or based on an optional trip id parameter.
-   * @param params optional parameters to send with the request
-   * @param headers headers to add to the http options object
-   */
-  fetch(params: any, headers: any): void {
-    const endpoint = 'trip-questions';
-    const options = {
-      headers: new HttpHeaders(headers),
-    };
-    this.faq$ = this.api.get(endpoint, params, options).pipe(
-      map((faqJson: any) => {
-        // Sort the guides and map them to Guide models
-        return faqJson
-          .sort((a: any, b: any) => a.updated_at - b.updated_at)
-          .map((faq: any) => new Faq(faq));
-      })
-    );
-  }
-
-  /** Display a dialog to let users send QAs */
-  openDialog(): void {
-    if (!this.auth.authenticated || !this.auth.getCredentials().accessToken) {
-      this.logger.error('Not logged in user had send QA option', this.auth);
-      return;
-    }
-    const dialogRef = this.dialog.open(PostQaDialogComponent);
-    dialogRef.afterClosed().subscribe((res: boolean) => {
-      if (res) {
-        this.logger.log('User posted new QA');
-        const headers = {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.auth.getCredentials().accessToken}`,
-        };
-        this.fetch(null, headers);
-      } else {
-        this.logger.debug('No QA posted');
-      }
-    });
-  }
-}
-
-@Component({
-  selector: 'app-post-qa-dialog.component',
-  templateUrl: './post-qa-dialog.component.html',
-  styleUrls: ['./post-qa-dialog.component.scss'],
-})
-export class PostQaDialogComponent implements OnInit {
-  question: string;
-
-  constructor(
-    public dialogRef: MatDialogRef<PostQaDialogComponent>,
-    private auth: AuthService,
-    private api: ApiService,
-    private logger: NGXLogger,
-    private snackBar: MatSnackBar,
-    private translate: TranslateService
-  ) {}
-
-  ngOnInit(): void {
-    this.question = '';
-  }
-
-  /** Post the question to the server. */
-  send() {
-    const q = this.question || this.question.trim();
-    if (q === '') {
-      this.logger.debug('Empty question body, cancelling');
-      this.dialogRef.close(false);
-      return;
-    }
-    this.logger.debug('Posting new QA: ' + q);
-    const endpoint = 'trip-questions';
-    const options = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: ' Bearer ' + this.auth.getCredentials().accessToken,
-      }),
-    };
-    const params = { question: q };
-    this.api.post(endpoint, params, options).subscribe(
-      (res: any) => {
-        this.logger.debug('Question received correctly');
-        this.snackBar.open(this.translate.instant('QUESTION_RECEIVED'), null, {
-          duration: 3000,
-        });
-        this.dialogRef.close(true);
-      },
-      (err: any) => {
-        this.logger.error('Error posting new QA', {
-          question: q,
-          auth: this.auth.getCredentials(),
-          res: err,
-        });
-        this.snackBar.open(
-          this.translate.instant('SERVER_ERROR_TRY_LATER'),
-          null,
-          {
-            duration: 3000,
-          }
-        );
-        this.dialogRef.close(false);
-      },
-      () => {
-        // Reset the question in case users want to ask multiple QAs.
-        this.question = '';
-      }
-    );
-  }
-
-  /** Close the dialog on cancel button click. */
-  cancel(): void {
-    this.logger.debug('Dialog cancelled by user');
-    this.dialogRef.close(false);
   }
 }
--- src/app/pages/faq/faq.module.ts
+++ src/app/pages/faq/faq.module.ts
@@ -1,37 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { FaqRoutingModule } from './faq-routing.module';
-import { FaqComponent, PostQaDialogComponent } from './faq.component';
-
-import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
-import { LoginRequiredMessageModule } from 'src/app/components/login-required-message/login-required-message.module';
-import { NoItemsNotificationModule } from 'src/app/components/no-items-notification/no-items-notification.module';
+import { FaqComponent } from './faq.component';
 
 @NgModule({
-  declarations: [FaqComponent, PostQaDialogComponent],
-  imports: [
-    CommonModule,
-    FaqRoutingModule,
-    FormsModule,
-    LoadingSpinnerContentModule,
-    LoginRequiredMessageModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatSnackBarModule,
-    NoItemsNotificationModule,
-    TranslateModule,
-  ],
+  declarations: [FaqComponent],
+  imports: [CommonModule, FaqRoutingModule, TranslateModule],
 })
 export class FaqModule {}
