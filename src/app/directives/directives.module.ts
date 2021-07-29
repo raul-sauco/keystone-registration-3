8c111c940e42837282b73dee0bb4d2945fa557d9
Raul Sauco <sauco.raul@gmail.com>
2021-07-29 12:22:22 +0200

    Removed unused contenteditable directive

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordMatchValidatorDirective } from './password-match-validator.directive';
 import { UniqueUsernameValidatorDirective } from './unique-username-validator.directive';
-import { ContenteditableDirective } from './contenteditable.directive';
 
 @NgModule({
   declarations: [
     PasswordMatchValidatorDirective,
     UniqueUsernameValidatorDirective,
-    ContenteditableDirective,
   ],
   imports: [CommonModule],
-  exports: [ContenteditableDirective],
+  exports: [],
 })
 export class DirectivesModule {}
