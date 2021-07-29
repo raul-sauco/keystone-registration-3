b4586a575ce887ecad6255026f6742f06a16c59a
Raul Sauco <sauco.raul@gmail.com>
2021-07-29 12:41:30 +0200

    Fixed test errors, no passing

--- src/app/directives/unique-username-validator.directive.spec.ts
+++ src/app/directives/unique-username-validator.directive.spec.ts
@@ -1,8 +1,30 @@
-import { UniqueUsernameValidatorDirective } from './unique-username-validator.directive';
+import { UsernameService } from './../services/username/username.service';
+import { waitForAsync, TestBed } from '@angular/core/testing';
+import {
+  UniqueUsernameValidator,
+  UniqueUsernameValidatorDirective,
+} from './unique-username-validator.directive';
 
 describe('UniqueUsernameValidatorDirective', () => {
+  let mockUsernameService;
+  beforeEach(() => {
+    mockUsernameService = jasmine.createSpyObj(['usernameService']);
+    mockUsernameService.isUsernameTaken.and.returnValue(false);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: UsernameService,
+          useValue: jasmine.createSpyObj('mockUsernameService', [
+            'isUsernameTaken',
+          ]),
+        },
+      ],
+    });
+  });
   it('should create an instance', () => {
-    const directive = new UniqueUsernameValidatorDirective();
+    const directive = new UniqueUsernameValidatorDirective(
+      new UniqueUsernameValidator(TestBed.inject(UsernameService))
+    );
     expect(directive).toBeTruthy();
   });
 });
