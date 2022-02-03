67d03d93821ac6f530e8d44e51c8f6c945d9ebad
Raul Sauco <sauco.raul@gmail.com>
2022-02-03 09:54:24 +0100

    Display payment QR codes

--- src/app/pages/payments/payment-qr-codes/payment-qr-codes.component.ts
+++ src/app/pages/payments/payment-qr-codes/payment-qr-codes.component.ts
@@ -1,5 +1,8 @@
+import { GlobalsService } from './../../../services/globals/globals.service';
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
+import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -9,13 +12,31 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./payment-qr-codes.component.scss'],
 })
 export class PaymentQrCodesComponent implements OnInit {
+  images$!: Observable<any>;
+  staticUrl: string;
+
   constructor(
     private api: ApiService,
     private auth: AuthService,
-    private logger: NGXLogger
-  ) {}
+    private logger: NGXLogger,
+    globals: GlobalsService
+  ) {
+    this.staticUrl = globals.getResUrl();
+  }
 
   ngOnInit(): void {
     this.logger.debug('PaymentQrCodes on init');
+    this.fetchQrCodes();
+  }
+
+  fetchQrCodes(): void {
+    const endpoint = 'trip-direct-payment-qr-codes';
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: ' Bearer ' + this.auth.getCredentials()?.accessToken,
+      }),
+    };
+    this.images$ = this.api.get(endpoint, null, options);
   }
 }
