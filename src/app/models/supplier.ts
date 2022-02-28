742becd0e7148a1b161bc1901297ab00b002c9d0
Raul Sauco <sauco.raul@gmail.com>
2022-02-28 20:56:09 +0100

    Add supplier c-trip link

--- src/app/models/supplier.ts
+++ src/app/models/supplier.ts
@@ -8,11 +8,20 @@ export class Supplier {
   name?: string;
   nameZh?: string;
   images?: Image[];
+  phone?: string;
+  address?: string;
+  addressZh?: string;
+  cTripUrl?: string;
+
   constructor(supplierJson: {
     id: number;
     name: string;
     name_zh: string;
     images: any;
+    phone?: string;
+    address?: string;
+    address_zh?: string;
+    c_trip_url?: string;
   }) {
     this.id = supplierJson.id;
     this.name = supplierJson.name;
@@ -20,5 +29,9 @@ export class Supplier {
     if (supplierJson.images) {
       this.images = supplierJson.images.map((i: any) => new Image(i));
     }
+    this.phone = supplierJson.phone;
+    this.address = supplierJson.address;
+    this.addressZh = supplierJson.address_zh;
+    this.cTripUrl = supplierJson.c_trip_url;
   }
 }
