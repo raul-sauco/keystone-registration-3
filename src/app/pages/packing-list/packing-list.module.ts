7bd501eb62fd0562f5fb2684719cc38ace0f6b1b
Raul Sauco <sauco.raul@gmail.com>
2022-03-11 15:55:56 +0100

    Add admin banner to packing list component

--- src/app/pages/packing-list/packing-list.module.ts
+++ src/app/pages/packing-list/packing-list.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { PackingListRoutingModule } from './packing-list-routing.module';
-import { PackingListComponent } from './packing-list.component';
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { MarkdownModule } from 'ngx-markdown';
-
-import { PackingListItemModule } from 'src/app/components/packing-list-item/packing-list-item.module';
+import { AdminBannerModule } from 'src/app/components/admin-banner/admin-banner.module';
 import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
 import { LoginRequiredMessageModule } from 'src/app/components/login-required-message/login-required-message.module';
-
+import { PackingListItemModule } from 'src/app/components/packing-list-item/packing-list-item.module';
+import { PackingListRoutingModule } from './packing-list-routing.module';
+import { PackingListComponent } from './packing-list.component';
 
 @NgModule({
   declarations: [PackingListComponent],
   imports: [
+    AdminBannerModule,
     CommonModule,
     PackingListRoutingModule,
     MatIconModule,
@@ -26,7 +25,7 @@ import { LoginRequiredMessageModule } from 'src/app/components/login-required-me
     TranslateModule,
     MarkdownModule.forChild(),
     LoadingSpinnerContentModule,
-    LoginRequiredMessageModule
-  ]
+    LoginRequiredMessageModule,
+  ],
 })
-export class PackingListModule { }
+export class PackingListModule {}
