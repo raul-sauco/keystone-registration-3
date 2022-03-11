8c97e6b04ef63dc8367f7dcd57fea2694d83f929
Raul Sauco <sauco.raul@gmail.com>
2022-03-11 16:06:27 +0100

    Add admin banner to guides component

--- src/app/pages/guides/guides.module.ts
+++ src/app/pages/guides/guides.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
+import { AdminBannerModule } from 'src/app/components/admin-banner/admin-banner.module';
+import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
+import { LoginRequiredMessageModule } from 'src/app/components/login-required-message/login-required-message.module';
 import { GuidesRoutingModule } from './guides-routing.module';
 import { GuidesComponent } from './guides.component';
-import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
-import { MarkdownModule } from 'ngx-markdown';
-import { LoginRequiredMessageModule } from 'src/app/components/login-required-message/login-required-message.module';
-
 
 @NgModule({
   declarations: [GuidesComponent],
   imports: [
+    AdminBannerModule,
     CommonModule,
     GuidesRoutingModule,
     LoadingSpinnerContentModule,
     LoginRequiredMessageModule,
-    MarkdownModule.forChild()
-  ]
+    MarkdownModule.forChild(),
+  ],
 })
-export class GuidesModule { }
+export class GuidesModule {}
