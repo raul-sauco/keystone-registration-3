9b29b8217e1f7a13bb92a53d0be964062fe1ec45
Raul Sauco <sauco.raul@gmail.com>
2022-03-23 10:16:04 +0100

    Improved the styles of supplier item

--- src/app/components/supplier-item/supplier-item.component.ts
+++ src/app/components/supplier-item/supplier-item.component.ts
@@ -1,8 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-
-import { Supplier } from './../../models/supplier';
-import { GlobalsService } from 'src/app/services/globals/globals.service';
 import { TranslateService } from '@ngx-translate/core';
+import { Supplier } from 'src/app/models/supplier';
+import { GlobalsService } from 'src/app/services/globals/globals.service';
 
 @Component({
   selector: 'app-supplier-item',
--- src/app/components/supplier-item/supplier-item.module.ts
+++ src/app/components/supplier-item/supplier-item.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 import { SupplierItemComponent } from './supplier-item.component';
 
 @NgModule({
   declarations: [SupplierItemComponent],
   exports: [SupplierItemComponent],
-  imports: [CommonModule, MatButtonModule, MatCardModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    TranslateModule,
+  ],
 })
 export class SupplierItemModule {}
