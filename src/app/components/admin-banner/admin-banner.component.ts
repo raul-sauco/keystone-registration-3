ed0a68a58ad425ee833fb08ccc98a789c40c9b3b
Raul Sauco <sauco.raul@gmail.com>
2022-03-24 10:01:54 +0100

    improve admin banner no trip selected visibility

--- src/app/components/admin-banner/admin-banner.component.ts
+++ src/app/components/admin-banner/admin-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
 import { Trip } from 'src/app/models/trip';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -15,20 +16,14 @@ export class AdminBannerComponent implements OnInit {
 
   constructor(
     private logger: NGXLogger,
+    private translate: TranslateService,
     public tripService: TripSwitcherService,
     public auth: AuthService
-  ) {}
-
-  ngOnInit(): void {
-    // TODO remove placeholder logic
-    this.logger.debug('AdminBannerComponent OnInit');
-    this.trip = new Trip({ id: 1, name: 'Placeholder Trip name' });
+  ) {
+    this.lang = this.translate.currentLang;
   }
 
-  /**
-   * Let a school admin navigate to the select trip page.
-   */
-  browseAvailableTrips(): void {
-    console.log('TODO: Navigate to select trip page');
+  ngOnInit(): void {
+    this.logger.debug('AdminBannerComponent OnInit');
   }
 }
--- src/app/components/admin-banner/admin-banner.module.ts
+++ src/app/components/admin-banner/admin-banner.module.ts
@@ -1,20 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AdminBannerComponent } from './admin-banner.component';
 
 @NgModule({
   declarations: [AdminBannerComponent],
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatCardModule,
-    RouterModule,
-    TranslateModule,
-  ],
+  imports: [CommonModule, MatButtonModule, RouterModule, TranslateModule],
   exports: [AdminBannerComponent],
 })
 export class AdminBannerModule {}
