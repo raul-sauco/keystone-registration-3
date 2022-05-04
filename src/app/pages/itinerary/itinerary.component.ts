f2e01741d4150213d53c1628f6b707af6b48d03e
Raul Sauco <sauco.raul@gmail.com>
2022-05-04 14:09:31 +0200

    Display different itineraries for admins and students

--- src/app/pages/itinerary/itinerary.component.ts
+++ src/app/pages/itinerary/itinerary.component.ts
@@ -1,9 +1,12 @@
 import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
 import { Observable, of } from 'rxjs';
+import { ActivityGroup } from 'src/app/models/activityGroup';
+import { ActivityGroupService } from 'src/app/services/activity-group/activity-group.service';
 import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { GlobalsService } from 'src/app/services/globals/globals.service';
@@ -17,18 +20,23 @@ import { TripSwitcherService } from 'src/app/services/trip-switcher/trip-switche
   encapsulation: ViewEncapsulation.None,
 })
 export class ItineraryComponent implements OnInit {
+  showPDF: boolean = true; // True for students and not logged in users
+  /** used by the template to iterate a collection */
+  activityGroups: ActivityGroup[] | null = null;
   itineraries$: Observable<any> | null = null;
   url: string;
   lang: string;
   needsLogin = false;
 
   constructor(
+    private activityGroupService: ActivityGroupService,
     private api: ApiService,
     private auth: AuthService,
     private globals: GlobalsService,
     private logger: NGXLogger,
     private route: ActivatedRoute,
     private routeStateService: RouteStateService,
+    private snackBar: MatSnackBar,
     private translate: TranslateService,
     private tripSwitcher: TripSwitcherService
   ) {
@@ -59,6 +67,9 @@ export class ItineraryComponent implements OnInit {
         // No trip id router parameter
         this.auth.checkAuthenticated().then((res: boolean) => {
           if (res) {
+            if (this.auth.isSchoolAdmin || this.auth.isTeacher) {
+              this.showPDF = false;
+            }
             if (this.auth.isSchoolAdmin) {
               if (this.tripSwitcher.selectedTrip) {
                 this.fetch(`${this.tripSwitcher.selectedTrip.id}`);
@@ -77,7 +88,21 @@ export class ItineraryComponent implements OnInit {
     });
   }
 
+  /**
+   * Fetch the data to be displayed. Data fetched depends on the user type.
+   * Teachers and school admins will see a list of events arranged by activity group and date.
+   * Other participants will see a PDF brochure uploaded by trip management.
+   * @param tripId
+   */
   fetch(tripId?: string): void {
+    if (this.showPDF) {
+      this.fetchPdfData(tripId);
+    } else {
+      this.fetchActivityGroupData(tripId);
+    }
+  }
+
+  fetchPdfData(tripId?: string): void {
     const endpoint =
       'files?tagged=itinerary' + (tripId ? `&trip-id=${tripId}` : '');
     const options = {
@@ -87,5 +112,26 @@ export class ItineraryComponent implements OnInit {
       }),
     };
     this.itineraries$ = this.api.get(endpoint, null, options);
+  }
+
+  fetchActivityGroupData(tripId?: string): void {
+    this.activityGroupService.fetchActivityGroups(tripId);
+
+    // Subscribe to the ActivityGroupService Subject
+    this.activityGroupService.activityGroup$.subscribe({
+      next: (resp) => {
+        this.activityGroups = resp;
+      },
+      error: (err: string) => {
+        // Notify the user of the error
+        const snackBarRef = this.snackBar.open(err, 'Retry', {
+          duration: 5000,
+        });
+
+        snackBarRef.onAction().subscribe(() => {
+          this.activityGroupService.fetchActivityGroups(tripId);
+        });
+      },
+    });
   }
 }
