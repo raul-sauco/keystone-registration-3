ad50fa96e75f0d968900835ba8aea2bfc7bba654
Raul Sauco <sauco.raul@gmail.com>
2023-04-07 15:51:57 +0200

    Show different help content for payment trips

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -1,9 +1,11 @@
 import { HttpHeaders } from '@angular/common/http';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
 import { Observable } from 'rxjs';
+import { PaymentInfo } from 'src/app/models/paymentInfo';
 import { ApiService } from 'src/app/services/api/api.service';
+import { PaymentService } from 'src/app/services/payment/payment.service';
 
 @Component({
   selector: 'app-help',
@@ -11,29 +13,43 @@ import { ApiService } from 'src/app/services/api/api.service';
   styleUrls: ['./help.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class HelpComponent implements OnInit {
+export class HelpComponent implements OnInit, OnDestroy {
   content$!: Observable<any>;
   lang!: string;
+  private isPaymentEnabled: boolean = false;
+  private documentId: string = '133';
 
   constructor(
     private api: ApiService,
     private logger: NGXLogger,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private paymentService: PaymentService
   ) {}
 
   ngOnInit(): void {
     this.logger.debug('HelpComponent OnInit');
     this.lang = this.translate.currentLang.includes('zh') ? 'zh' : 'en';
     this.fetchContent();
+    this.paymentService.paymentInfo$.subscribe((paymentInfo: PaymentInfo) => {
+      // For trips with payment enabled, show a more comprehensive help page.
+      if (paymentInfo.required) {
+        this.documentId = '131';
+        this.fetchContent();
+      }
+    });
   }
 
   fetchContent(): void {
-    const endpoint = 'documents/131';
+    const endpoint = `documents/${this.documentId}`;
     const options = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
     };
     this.content$ = this.api.get(endpoint, null, options);
+  }
+
+  ngOnDestroy(): void {
+    this.paymentService.paymentInfo$.unsubscribe();
   }
 }
