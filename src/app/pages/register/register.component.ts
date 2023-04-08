1791fcba64f82a4fae4193ac59c3b078ae86415b
Raul Sauco <sauco.raul@gmail.com>
2023-04-08 17:53:01 +0200

    Navigate home after registration

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -164,7 +164,7 @@ export class RegisterComponent implements OnInit {
     this.logger.debug('RegisterComponent registration successful');
     const dialogRef = this.dialog.open(RegistrationSuccessDialogComponent);
     dialogRef.afterClosed().subscribe(() => {
-      this.router.navigateByUrl('/waiver');
+      this.router.navigateByUrl('/home');
     });
   }
 }
