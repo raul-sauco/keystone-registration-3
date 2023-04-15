a8d7ce39907d2783122eb66f7468d29a065ebb49
Raul Sauco <sauco.raul@gmail.com>
2023-04-15 18:35:36 +0200

    Handle trip context switches for school admins

--- src/app/services/trip/trip.service.spec.ts
+++ src/app/services/trip/trip.service.spec.ts
@@ -1,41 +1,68 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
-import { Subject } from 'rxjs';
-import { Trip } from 'src/app/models/trip';
-import { Spied } from '../../interfaces/spied';
-import { Credentials } from '../../models/credentials';
-import { AuthService } from '../auth/auth.service';
-import { StorageService } from '../storage/storage.service';
+import { Subject, of } from 'rxjs';
+
+import { Spied } from '@interfaces/spied';
+import { Credentials } from '@models/credentials';
+import { Trip } from '@models/trip';
+import { ApiService } from '@services/api/api.service';
+import { AuthService } from '@services/auth/auth.service';
+import { StorageService } from '@services/storage/storage.service';
+import { TripSwitcherService } from '@services/trip-switcher/trip-switcher.service';
 import { TripService } from './trip.service';
 
 describe('TripService', () => {
   const tripName = 'Test trip name';
+  const tripData = {
+    id: 123,
+    name_en: 'name',
+    name_zh: 'ming',
+    accept_direct_payment: 0,
+  };
+  const tripA = new Trip({
+    id: 1,
+    name_en: 'tripA',
+    name_zh: 'tripAzh',
+    accept_direct_payment: 0,
+  });
+  const tripB = new Trip({
+    id: 2,
+    name_en: 'tripB',
+    name_zh: 'tripBzh',
+    accept_direct_payment: 0,
+  });
+  const credentialsA = new Credentials({
+    userName: 'test',
+    accessToken: 'test-token',
+    type: 8, // School admin type
+    studentId: undefined,
+  });
   let service: TripService;
+  let apiServiceSpy: Spied<ApiService>;
   let authServiceSpy: Spied<AuthService>;
   let authSubject: Subject<boolean> = new Subject<boolean>();
   let loggerSpy: Spied<NGXLogger>;
   let storageServiceSpy: Spied<StorageService>;
   let translateServiceSpy: Spied<TranslateService>;
+  let tripSwitcherSpy: Spied<TripSwitcherService>;
 
   beforeEach(() => {
     authSubject = new Subject<boolean>();
+    apiServiceSpy = jasmine.createSpyObj('ApiServiceSpy', {
+      get: of(new Trip(tripData)),
+    });
     authServiceSpy = jasmine.createSpyObj(
       'AuthServiceSpy',
       {
-        getCredentials: new Credentials({
-          userName: 'test',
-          accessToken: 'test-token',
-          type: 8, // School admin type
-          studentId: undefined,
-        }),
+        getCredentials: credentialsA,
         checkAuthenticated: Promise.resolve(true),
       },
       { auth$: authSubject }
     );
     loggerSpy = jasmine.createSpyObj('NGXLoggerSpy', {
       debug: null,
+      trace: null,
     });
     storageServiceSpy = jasmine.createSpyObj('StorageServiceSpy', {
       get: Promise.resolve(tripName),
@@ -47,12 +74,21 @@ describe('TripService', () => {
       {},
       { currentLang: 'en' }
     );
+    tripSwitcherSpy = jasmine.createSpyObj(
+      'TripSwitcherSpy',
+      {
+        selectTrip: true,
+      },
+      { selectedTrip$: of(tripA) }
+    );
     TestBed.configureTestingModule({
       providers: [
+        { provide: ApiService, useValue: apiServiceSpy },
         { provide: AuthService, useValue: authServiceSpy },
-        { provide: StorageService, useValue: storageServiceSpy },
         { provide: NGXLogger, useValue: loggerSpy },
+        { provide: StorageService, useValue: storageServiceSpy },
         { provide: TranslateService, useValue: translateServiceSpy },
+        { provide: TripSwitcherService, useValue: tripSwitcherSpy },
       ],
     });
     service = TestBed.inject(TripService);
@@ -66,7 +102,6 @@ describe('TripService', () => {
   it('should subscribe to authentication status changes', () => {
     expect(loggerSpy.debug).toHaveBeenCalledTimes(1);
     authSubject.next(true);
-    expect(loggerSpy.debug).toHaveBeenCalledTimes(2);
     expect(loggerSpy.debug).toHaveBeenCalledWith(
       'Updated authentication status true'
     );
@@ -96,26 +131,22 @@ describe('TripService', () => {
     expect(service.type).toEqual(values.type);
   });
 
-  it('should set and remove trip', (done: DoneFn) => {
+  it('should set and remove trip for school admin', (done: DoneFn) => {
     // Expect the empty trip name, then the set name, then remove name.
-    const marbles = ['', 'name', ''];
+    const marbles = ['', 'tripA', ''];
     let calls = 0;
     expect(loggerSpy.debug).toHaveBeenCalledTimes(1);
-    const tripData = {
-      id: 123,
-      name_en: 'name',
-      name_zh: 'ming',
-      accept_direct_payment: 0,
-    };
+    // Subscribe to updates on the trip name.
     service.tripName$.subscribe((name) => {
-      expect(name).toEqual(marbles[calls]);
+      expect(name).toEqual(
+        marbles[calls],
+        `Expected call #${calls} to equal ${marbles[calls]} but found ${name}`
+      );
       calls++;
     });
-    service.setTrip(tripData);
-    expect(storageServiceSpy.set).toHaveBeenCalledWith(
-      'KEYSTONE_ADVENTURES_CURRENT_TRIP_DATA',
-      new Trip(tripData)
-    );
+    // This should trigger a call to set trip.
+    authSubject.next(true);
+    // This should trigger a call to clear.
     authSubject.next(false);
     expect(storageServiceSpy.remove).toHaveBeenCalledWith(
       'KEYSTONE_ADVENTURES_CURRENT_TRIP_DATA'
--- src/app/services/trip/trip.service.ts
+++ src/app/services/trip/trip.service.ts
@@ -7,44 +7,69 @@ import { Credentials } from '@models/credentials';
 import { Trip } from '@models/trip';
 import { AuthService } from '@services/auth/auth.service';
 import { StorageService } from '@services/storage/storage.service';
+import { TripSwitcherService } from '@services/trip-switcher/trip-switcher.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TripService {
   private TRIP_DATA_STORAGE_KEY = 'KEYSTONE_ADVENTURES_CURRENT_TRIP_DATA';
-  public tripName$: BehaviorSubject<string> = new BehaviorSubject('');
+  private _tripName$: BehaviorSubject<string> = new BehaviorSubject('');
   id!: number;
   name!: string;
   code!: string;
   type!: string;
 
   constructor(
+    // private api: ApiService,
     private auth: AuthService,
     private storageService: StorageService,
     private logger: NGXLogger,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private tripSwitcherService: TripSwitcherService
   ) {
     this.init();
+  }
+
+  get tripName$() {
+    return this._tripName$;
   }
 
   init() {
     this.logger.debug('TripService::init()');
     // Subscribe to authentication updates.
     this.auth.auth$.subscribe((authStatus: boolean) => {
-      this.logger.debug(`Updated authentication status ${authStatus}`);
-      if (!authStatus) {
-        this.clear();
-      } else {
-        if (this.auth.isStudent || this.auth.isTeacher) {
-          // If we received auth$ true we should have credentials.
-          const cred: Credentials = this.auth.getCredentials()!;
-        } else {
-          // TODO: Subscribe to updates to TripSwitcher service when
-          // school admins select a trip.
-        }
-      }
+      this.handleAuthStatusChange(authStatus);
     });
+  }
+
+  /**
+   * Handle changes to the application authentication service status, could be
+   * a login or a logout.
+   * @param authStatus boolean
+   */
+  private handleAuthStatusChange(authStatus: boolean) {
+    this.logger.debug(`Updated authentication status ${authStatus}`);
+    if (!authStatus) {
+      this.clear();
+    } else {
+      if (this.auth.isStudent || this.auth.isTeacher) {
+        // If we received auth$ true we should have credentials.
+        const cred: Credentials | undefined = this.auth.getCredentials();
+        if (cred) {
+          this.fetch(cred);
+        } else {
+          this.logger.warn('Expected credentials to exists and be valid');
+        }
+      } else {
+        this.logger.debug(
+          'Detected school admin user, subscribing to TripSwitcher updates'
+        );
+        this.tripSwitcherService.selectedTrip$.subscribe((trip) =>
+          this.setTrip(trip)
+        );
+      }
+    }
   }
 
   /**
@@ -52,7 +77,7 @@ export class TripService {
    */
   clear() {
     this.logger.debug('TripService::clear()');
-    this.tripName$.next('');
+    this._tripName$.next('');
     this.storageService.remove(this.TRIP_DATA_STORAGE_KEY);
   }
 
@@ -60,10 +85,9 @@ export class TripService {
    * Set the trip data.
    * @param tripData any
    */
-  setTrip(tripData: any): void {
-    const trip = new Trip(tripData);
+  private setTrip(trip: Trip): void {
     this.storageService.set(this.TRIP_DATA_STORAGE_KEY, trip);
-    this.tripName$.next(trip.getName(this.translate.currentLang));
+    this._tripName$.next(trip.getName(this.translate.currentLang));
   }
 
   /**
@@ -85,5 +109,13 @@ export class TripService {
     this.type = data.type;
     // todo check if values can be used
     return true;
+  }
+
+  /**
+   * Fetch trip data from the API.
+   * @param cred Credentials The authenticated user credentials to fetch for.
+   */
+  private fetch(cred: Credentials): void {
+    this.logger.debug('TripService fetching from API');
   }
 }
