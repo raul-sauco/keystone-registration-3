4d1566566d6eb5e9c63023065f2eba71f551b509
Raul Sauco <sauco.raul@gmail.com>
2023-05-21 12:34:14 +0200

    Fetch waiver content from webapp documents

--- src/app/pages/waiver/waiver-content/waiver-content.component.ts
+++ src/app/pages/waiver/waiver-content/waiver-content.component.ts
@@ -2,15 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
 
+import { HttpHeaders } from '@angular/common/http';
+import { ApiService } from '@services/api/api.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-waiver-content',
   templateUrl: './waiver-content.component.html',
   styleUrls: ['./waiver-content.component.scss'],
 })
 export class WaiverContentComponent implements OnInit {
-  constructor(public translate: TranslateService, private logger: NGXLogger) {}
+  content$!: Observable<any>;
+
+  constructor(
+    private logger: NGXLogger,
+    public api: ApiService,
+    public translate: TranslateService
+  ) {}
 
   ngOnInit(): void {
     this.logger.debug('WaiverContentComponent OnInit');
+    this.fetchContents();
+  }
+  /**
+   * Fetch content that needs to be displayed in the UI.
+   */
+  fetchContents() {
+    const options = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+    this.content$ = this.api.get('documents/75', null, options);
   }
 }
--- src/app/pages/waiver/waiver.module.ts
+++ src/app/pages/waiver/waiver.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { TranslateModule } from '@ngx-translate/core';
 
+import { LoadingSpinnerContentModule } from '@components/loading-spinner-content/loading-spinner-content.module';
+import { LoginRequiredMessageModule } from '@components/login-required-message/login-required-message.module';
+import { MarkdownModule } from 'ngx-markdown';
+import { WaiverContentComponent } from './waiver-content/waiver-content.component';
 import { WaiverRoutingModule } from './waiver-routing.module';
 import { WaiverComponent } from './waiver.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
-import { LoginRequiredMessageModule } from 'src/app/components/login-required-message/login-required-message.module';
-import { WaiverContentComponent } from './waiver-content/waiver-content.component';
 
 @NgModule({
   declarations: [WaiverComponent, WaiverContentComponent],
@@ -23,6 +23,7 @@ import { WaiverContentComponent } from './waiver-content/waiver-content.componen
     FormsModule,
     LoadingSpinnerContentModule,
     LoginRequiredMessageModule,
+    MarkdownModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
