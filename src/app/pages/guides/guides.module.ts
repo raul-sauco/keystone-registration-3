bc20425ed0043e045b445b2437dc6a29b8f86da2
Raul Sauco <sauco.raul@gmail.com>
2023-09-21 16:12:33 +0200

    Display staff not confirmed message

--- src/app/pages/guides/guides.module.ts
+++ src/app/pages/guides/guides.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
 import { MarkdownModule } from 'ngx-markdown';
-import { AdminBannerModule } from 'src/app/components/admin-banner/admin-banner.module';
-import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
-import { LoginRequiredMessageModule } from 'src/app/components/login-required-message/login-required-message.module';
+
+import { AdminBannerModule } from '@components/admin-banner/admin-banner.module';
+import { LoadingSpinnerContentModule } from '@components/loading-spinner-content/loading-spinner-content.module';
+import { LoginRequiredMessageModule } from '@components/login-required-message/login-required-message.module';
 import { GuidesRoutingModule } from './guides-routing.module';
 import { GuidesComponent } from './guides.component';
 
@@ -16,6 +19,8 @@ import { GuidesComponent } from './guides.component';
     LoadingSpinnerContentModule,
     LoginRequiredMessageModule,
     MarkdownModule.forChild(),
+    MatCardModule,
+    TranslateModule,
   ],
 })
 export class GuidesModule {}
