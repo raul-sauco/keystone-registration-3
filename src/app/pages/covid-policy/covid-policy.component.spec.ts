655c5a1c8dec7f6c999c883768e62388c9655238
Raul Sauco <sauco.raul@gmail.com>
2023-12-13 14:25:59 +0100

    wip: Simplify test dependencies using NgMocks

--- src/app/pages/covid-policy/covid-policy.component.spec.ts
+++ src/app/pages/covid-policy/covid-policy.component.spec.ts
@@ -1,57 +1,14 @@
-import { HttpClient } from '@angular/common/http';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import {
-  TranslateLoader,
-  TranslateModule,
-  TranslateService,
-} from '@ngx-translate/core';
-import { LoggerTestingModule } from 'ngx-logger/testing';
-import { HttpLoaderFactory } from 'src/app/app.module';
-import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
+import { MockBuilder, MockRender } from 'ng-mocks';
+
 import { CovidPolicyComponent } from './covid-policy.component';
 
 describe('CovidPolicyComponent', () => {
-  let component: CovidPolicyComponent;
-  let fixture: ComponentFixture<CovidPolicyComponent>;
-  let translate: TranslateService;
-  let http: HttpTestingController;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [],
-      imports: [
-        CovidPolicyComponent,
-        HttpClientTestingModule,
-        LoadingSpinnerContentModule,
-        LoggerTestingModule,
-        RouterTestingModule,
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useFactory: HttpLoaderFactory,
-            deps: [HttpClient],
-          },
-        }),
-      ],
-    }).compileComponents();
-    translate = TestBed.inject(TranslateService);
-    translate.setDefaultLang('en');
-    translate.use('en');
-    http = TestBed.inject(HttpTestingController);
-  }));
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(CovidPolicyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    return MockBuilder(CovidPolicyComponent);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const fixture = MockRender(CovidPolicyComponent);
+    expect(fixture.point.componentInstance).toBeTruthy();
   });
 });
--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,35 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// It is important to keep the zone.js import before others. Used by fakeAsync, internally
+// used by ngMocks.
+// https://stackoverflow.com/a/68797535/2557030
 import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { ngMocks } from 'ng-mocks';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+  platformBrowserDynamicTesting(),
+  {
+    teardown: { destroyAfterEach: false },
+  }
 );
+// All methods in mock declarations and providers
+// will be automatically spied on their creation.
+// https://ng-mocks.sudo.eu/extra/auto-spy
+ngMocks.autoSpy('jasmine'); // or jest
+
+// ngMocks.defaultMock helps to customize mocks
+// globally. Therefore, we can avoid copy-pasting
+// among tests.
+// https://ng-mocks.sudo.eu/api/ngMocks/defaultMock
+// ngMocks.defaultMock(AuthService, () => ({
+//   isLoggedIn$: EMPTY,
+//   currentUser$: EMPTY,
+// }));
