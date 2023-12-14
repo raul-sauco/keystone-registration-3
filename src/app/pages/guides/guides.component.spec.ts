3516d0fb1a595338e3f6239b3b16f3d59033bc97
Raul Sauco <sauco.raul@gmail.com>
2023-12-14 06:49:15 +0100

    wip: Provide missing GuideComponent spec dependencies

--- src/app/pages/guides/guides.component.spec.ts
+++ src/app/pages/guides/guides.component.spec.ts
@@ -1,33 +1,36 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { GuidesComponent } from './guides.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { LoggerTestingModule } from 'ngx-logger/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LoggerTestingModule } from 'ngx-logger/testing';
 import { TranslateTestingModule } from 'ngx-translate-testing';
-import { LoadingSpinnerContentModule } from 'src/app/components/loading-spinner-content/loading-spinner-content.module';
+import { GuidesComponent } from './guides.component';
+
+import { AdminBannerModule } from '@components/admin-banner/admin-banner.module';
+import { LoadingSpinnerContentModule } from '@components/loading-spinner-content/loading-spinner-content.module';
 
 describe('GuidesComponent', () => {
   let component: GuidesComponent;
   let fixture: ComponentFixture<GuidesComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          HttpClientTestingModule,
-          LoggerTestingModule,
-          RouterTestingModule,
-          MatSnackBarModule,
-          LoadingSpinnerContentModule,
-          TranslateTestingModule.withTranslations({
-            en: require('src/assets/i18n/en.json'),
-          }),
-        ],
-        declarations: [GuidesComponent],
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AdminBannerModule,
+        HttpClientTestingModule,
+        LoggerTestingModule,
+        RouterTestingModule,
+        MatCardModule,
+        MatSnackBarModule,
+        LoadingSpinnerContentModule,
+        TranslateTestingModule.withTranslations({
+          en: require('src/assets/i18n/en.json'),
+        }),
+      ],
+      declarations: [GuidesComponent],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GuidesComponent);
