9dfe3e44d8a53dfd056c1492b88f651fb378c227
Raul Sauco <sauco.raul@gmail.com>
2024-01-20 21:57:24 +0100

    instant translate waiver content component

--- src/app/pages/waiver/waiver-content/waiver-content.component.ts
+++ src/app/pages/waiver/waiver-content/waiver-content.component.ts
@@ -1,8 +1,6 @@
-import { HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
-import { Observable, map } from 'rxjs';
 
 import { PaymentInfo } from '@models/paymentInfo';
 import { ApiService } from '@services/api/api.service';
@@ -13,35 +11,39 @@ import { PaymentService } from '@services/payment/payment.service';
   styleUrls: ['./waiver-content.component.scss'],
 })
 export class WaiverContentComponent implements OnInit {
-  content$!: Observable<any>;
+  document!: any;
+  content = signal(null);
 
   constructor(
     private logger: NGXLogger,
     private paymentService: PaymentService,
     public api: ApiService,
-    public translate: TranslateService
+    public translate: TranslateService,
   ) {}
 
   ngOnInit(): void {
     this.logger.debug('WaiverContentComponent::OnInit');
-    this.fetchContents();
+    this.fetchDocument();
+    this.translate.onLangChange.subscribe((_) => this.pushContent());
   }
 
-  fetchContents() {
-    const options = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    };
+  fetchDocument() {
     this.paymentService.paymentInfo$.subscribe({
       next: (paymentInfo: PaymentInfo) => {
         const endpoint = 'documents/' + (paymentInfo?.required ? '104' : '75');
-        this.content$ = this.api
-          .get(endpoint, null, options)
-          .pipe(
-            map((doc: any) =>
-              this.translate.currentLang.includes('zh') ? doc.text_zh : doc.text
-            )
-          );
+        this.api.get(endpoint).subscribe((doc) => {
+          this.document = doc;
+          this.pushContent();
+        });
       },
     });
+  }
+
+  pushContent() {
+    this.content.set(
+      this.translate.currentLang.includes('zh')
+        ? this.document.text_zh
+        : this.document.text,
+    );
   }
 }
