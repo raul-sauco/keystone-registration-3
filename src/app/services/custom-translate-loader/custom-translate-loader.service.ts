f77bc52ee5ff92546da3604c74aa8a0737a005c6
Raul Sauco <sauco.raul@gmail.com>
2024-04-13 06:40:51 +0200

    Fetch translations from static file instead of API

--- src/app/services/custom-translate-loader/custom-translate-loader.service.ts
+++ src/app/services/custom-translate-loader/custom-translate-loader.service.ts
@@ -12,8 +12,6 @@ export class CustomTranslationsLoader implements TranslateLoader {
   constructor(private http: HttpClient) {}
 
   public getTranslation(lang: String): Observable<any> {
-    return this.http.get(
-      `${environment.apiUrl}portal-translations?lang=${lang}`,
-    );
+    return this.http.get(`${environment.resUrl}portal/i18n/${lang}.json`);
   }
 }
