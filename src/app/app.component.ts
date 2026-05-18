db0f88fe2e8d5964d4cb4c7d668f79c055cafe3f
Raul Sauco <sauco.raul@gmail.com>
2026-05-18 06:43:10 +0200

    REF(app): Update navigation observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
 import { AsyncPipe, CommonModule, UpperCasePipe } from '@angular/common';
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, computed, inject } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,13 +11,9 @@ import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { LoggerModule, NGXLogger } from 'ngx-logger';
 import { MarkdownModule } from 'ngx-markdown';
-import { Observable, combineLatest, of } from 'rxjs';
-import { delay, filter, map, shareReplay, startWith, withLatestFrom } from 'rxjs/operators';
 
 import { AdminBannerModule } from '@components/admin-banner/admin-banner.module';
 import { AuthState } from '@models/auth-state';
-import { PaymentInfo } from '@models/paymentInfo';
-import { Student } from '@models/student';
 import { ApiService } from '@services/api/api.service';
 import { AuthService } from '@services/auth/auth.service';
 import { PaymentService } from '@services/payment/payment.service';
@@ -26,6 +22,7 @@ import { StudentService } from '@services/student/student.service';
 import { TripSwitcherService } from '@services/trip-switcher/trip-switcher.service';
 import { TripService } from '@services/trip/trip.service';
 import { LoadingSpinnerContentComponent } from '@components/loading-spinner-content/loading-spinner-content.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -193,7 +190,7 @@ export class AppComponent implements OnInit {
     } else {
       this.translate.use('en');
     }
-    if (this.auth.authenticated) {
+    if (this.auth.authenticated()) {
       // Set the user's preference in the backend, 1 => en, 2 => zh.
       this.api.patch('user-preferences/1', { value }).subscribe({
         next: (res: any) => this.logger.debug(res),
@@ -203,34 +200,35 @@ export class AppComponent implements OnInit {
   }
 
   /**
-   * Set a combine latest observer that uses multiple different observers to determine if the app
-   * should let the user navigate to all pages or only a restricted set.
+   * Computed signal to determine whether to let the user navigate to routes
+   * that should only be accessible after completing the registration steps
    */
-  setEnableFullNavigationObserver() {
-    this.logger.debug('AppComponent.setEnableFullNavigationObserver');
-    // Combined observable from several sources.
-    this.enableFullNavigation$ = combineLatest(
-      [
-        this.studentService.student$.pipe(startWith(null)),
-        this.paymentService.paymentInfo$.pipe(startWith(null)),
-        this.auth.auth$,
-      ],
-      (student: Student | null, paymentInfo: PaymentInfo | null, authState: AuthState) => {
-        if (
-          authState !== AuthState.Authenticated ||
-          student === null ||
-          paymentInfo === null ||
-          !student.waiverAccepted ||
-          (this.auth.isStudent && paymentInfo.required && !paymentInfo.paid)
-        ) {
-          this.logger.debug('AppComponent: Blocking full navigation');
-          return false;
-        }
-        this.logger.debug('AppComponent: Allowing full navigation');
-        return true;
-      },
-    );
-  }
+  readonly enableFullNavigation = computed(() => {
+    this.logger.debug('AppComponent: computing enableFullNavigation');
+    if (!this.auth.authenticated()) {
+      return false;
+    }
+
+    const student = this.student();
+    if (!student) {
+      return false;
+    }
+
+    const paymentInfo = this.paymentService.paymentInfo.value();
+    if (!paymentInfo) {
+      return false;
+    }
+
+    if (!student.waiverAccepted) {
+      return false;
+    }
+
+    if (this.auth.isStudent && paymentInfo.required && !paymentInfo.paid) {
+      return false;
+    }
+
+    return true;
+  });
 
   /** Logout the current application user */
   async logout() {
@@ -242,4 +240,7 @@ export class AppComponent implements OnInit {
       this.logger.warn('AppComponent error logging out', error);
     }
   }
+
+  // TODO: Transitional method while StudentService is still using Obserbables.
+  readonly student = toSignal(this.studentService.student$, { initialValue: null });
 }
