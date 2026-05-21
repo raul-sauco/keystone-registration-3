3a666487fa649dc2f378690318d30db19ca2e794
Raul Sauco <sauco.raul@gmail.com>
2026-05-21 06:59:24 +0200

    WIP: PaymentUploadedProof remove deprecated calls

--- src/app/pages/payments/payment-uploaded/payment-uploaded.component.ts
+++ src/app/pages/payments/payment-uploaded/payment-uploaded.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import {
   MatDialog,
-  MatDialogRef,
-  MatDialogTitle,
   MatDialogActions,
   MatDialogClose,
+  MatDialogRef,
+  MatDialogTitle,
 } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
 import { Subscription } from 'rxjs';
 import { Student } from 'src/app/models/student';
@@ -14,17 +18,12 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 import { GlobalsService } from 'src/app/services/globals/globals.service';
 import { PaymentService } from 'src/app/services/payment/payment.service';
 import { StudentService } from 'src/app/services/student/student.service';
-import { MatButton } from '@angular/material/button';
-import { TranslatePipe } from '@ngx-translate/core';
-import { AsyncPipe } from '@angular/common';
-import { MatIcon } from '@angular/material/icon';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-payment-uploaded',
   templateUrl: './payment-uploaded.component.html',
   styleUrls: ['./payment-uploaded.component.scss'],
-  imports: [MatButton, MatIcon, MatProgressSpinner, AsyncPipe, TranslatePipe],
+  imports: [MatButton, MatIcon, MatProgressSpinner, TranslatePipe],
 })
 export class PaymentUploadedComponent implements OnInit, OnDestroy {
   private auth = inject(AuthService);
@@ -40,7 +39,8 @@ export class PaymentUploadedComponent implements OnInit, OnDestroy {
   constructor() {
     const globals = inject(GlobalsService);
 
-    this.staticUrl = globals.getResUrl() + 'img/trip/pop/' + this.auth.credentials?.studentId + '/';
+    this.staticUrl =
+      globals.getResUrl() + 'img/trip/pop/' + this.auth.credentialsSignal()?.studentId + '/';
   }
 
   ngOnDestroy(): void {
